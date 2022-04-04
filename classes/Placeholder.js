88b26d4c6cbb02b75d8d146087bac340dddef129
baranyaf <baranya.florian@molaris.hu>
2022-04-04 19:46:51 +0200

    Placeholders have tiles

--- classes/Placeholder.js
+++ classes/Placeholder.js
@@ -2,10 +2,15 @@ export default class Placeholder {
   #placeHolderElem;
   #x;
   #y;
+  #tile;
 
   constructor(placeHolderElem, x, y) {
     this.#placeHolderElem = placeHolderElem;
     this.#x = x;
     this.#y = y;
+  }
+
+  get tile() {
+    return this.#tile;
   }
 }
